af493493c668d4604e2f9fbe7578611bc39a9f7a
Kenneth Horn <kennethhorn@Kenneths-MacBook-Air.local>
2021-07-17 23:39:18 -0700

    clock and calculator widgets are working

--- src/demos/JunkDrawer/WidgetCalc.js
+++ src/demos/JunkDrawer/WidgetCalc.js
@@ -1,11 +1,156 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Draggable from 'react-draggable'
 
 const WidgetCalc = () => {
+  const [ calcData, setCalcData ] = useState({
+    display: '0',
+    previous: null,
+    opperator: null,
+    clearOnPush: false,
+    memory: '0',
+  })
+
+  const pushButton = (btn) => {
+    if (calcData.clearOnPush) {
+      setCalcData({
+        ...calcData,
+        display: btn,
+        clearOnPush: false
+      })
+    } else {
+      let newCalcDisplay = calcData.display === "0" ? btn : calcData.display += btn
+      setCalcData({
+        ...calcData,
+        display: newCalcDisplay
+      })
+    }
+  }
+
+  const pushOpper = (op) => {
+    setCalcData({
+      ...calcData,
+      previous: calcData.display,
+      opperator: op,
+      clearOnPush: true
+    })
+  }
+
+  const pushEquals = () => {
+    let result = 0
+    if (calcData.opperator === "plus") result = Number(calcData.previous) + Number(calcData.display)
+    if (calcData.opperator === "minus") result = Number(calcData.previous) - Number(calcData.display)
+    if (calcData.opperator === "times") result = Number(calcData.previous) * Number(calcData.display)
+    if (calcData.opperator === "divide") result = Number(calcData.previous) / Number(calcData.display)
+    setCalcData({
+      ...calcData,
+      display: String(result),
+      previous: null,
+      opperator: null,
+      clearOnPush: false
+    })
+  }
+
+  const pushClear = () => {
+    setCalcData({
+      ...calcData,
+      display: "0",
+      previous: null,
+      opperator: null,
+      clearOnPush: false
+    })
+  }
+
+  const pushDot = () => {
+    if (calcData.display.indexOf('.') < 0) {
+      if (calcData.clearOnPush) {
+        setCalcData({
+          ...calcData,
+          display: "0.",
+          clearOnPush: false
+        })
+      } else {
+        let newCalcDisplay = calcData.display += "."
+        setCalcData({
+          ...calcData,
+          display: newCalcDisplay
+        })
+      }
+    }
+  }
+
+  const pushZero = () => {
+    if (calcData.display !== "0" && !calcData.clearOnPush) {
+      let newCalcDisplay = calcData.display += "0"
+      setCalcData({
+        ...calcData,
+        display: newCalcDisplay
+      })
+    }
+  }
+
+  const pushNeg = () => {
+    if (!calcData.clearOnPush) {
+      let newCalcDisplay = Number(calcData.display) * -1
+      setCalcData({
+        ...calcData,
+        display: String(newCalcDisplay)
+      })
+    }
+  }
+
+  const pushMem = () => {
+    setCalcData({
+      ...calcData,
+      memory: calcData.display
+    })
+  }
+
+  const pushRec = () => {
+    setCalcData({
+      ...calcData,
+      display: calcData.memory,
+      memory: "0"
+    })
+  }
   return (
     <Draggable>
       <div className="widget widget-calculator">
-        <h3>Calculator Goes Here</h3>
+
+        <div className="calc-row">
+          <input type="text" className="form-control display center-block text-right" readonly value={calcData.display} />
+        </div>
+
+        <div className="calc-row">
+          <button className='greyButton' onClick={ () => { pushButton('7') } }>7</button>
+          <button className='greyButton' onClick={ () => { pushButton('8') } }>8</button>
+          <button className='greyButton' onClick={ () => { pushButton('9') } }>9</button>
+          <button className='blueButton' onClick={ () => { pushOpper('plus') } }>+</button>
+          <button className='blueButton' onClick={ () => { pushOpper('times') } }>x</button>
+        </div>
+
+        <div className="calc-row">
+          <button className='greyButton' onClick={ () => { pushButton('4') } }>4</button>
+          <button className='greyButton' onClick={ () => { pushButton('5') } }>5</button>
+          <button className='greyButton' onClick={ () => { pushButton('6') } }>6</button>
+          <button className='blueButton' onClick={ () => { pushOpper('minus') } }>-</button>
+          <button className='blueButton' onClick={ () => { pushOpper('divide') } }>&divide;</button>
+        </div>
+
+        <div className="calc-row">
+          <button className='greyButton' onClick={ () => { pushButton('3') } }>3</button>
+          <button className='greyButton' onClick={ () => { pushButton('2') } }>2</button>
+          <button className='greyButton' onClick={ () => { pushButton('1') } }>1</button>
+          <button className='yellowButton' onClick={ () => { pushEquals() } }>=</button>
+          <button className='purpleButton' onClick={ () => { pushClear() } }>c</button>
+        </div>
+
+        <div className="calc-row">
+          <button className='darkButton' onClick={ () => { pushDot() } }>.</button>
+          <button className='greyButton' onClick={ () => { pushZero() } }>0</button>
+          <button className='darkButton' onClick={ () => { pushNeg() } }>+-</button>
+          <button className='greenButton' onClick={ () => { pushMem() } }>M</button>
+          <button className='redButton' onClick={ () => { pushRec() } }>R</button>
+        </div>
       </div>
     </Draggable>
   )
--- src/demos/JunkDrawer/WidgetClock.js
+++ src/demos/JunkDrawer/WidgetClock.js
@@ -1,11 +1,94 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Draggable from 'react-draggable'
 
 const WidgetClock = () => {
+  const [ currentTime, setCurrentTime ] = useState({
+    month: null,
+    day: null,
+    date: null,
+    hours: null,
+    hoursForClock: null,
+    minutes: null,
+    seconds: null,
+    ampm: null,
+  })
+
+  const setTime = () => {
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    const time = new Date()
+    const timeHours = time.getHours()
+    const day = time.getDay()
+    const month = time.getMonth()
+    
+    const timeToSet = {
+      month: months[month],
+      day: days[day],
+      date: time.getDate(),
+      hours: timeHours,
+      hoursForClock: timeHours % 12,
+      minutes: time.getMinutes(),
+      seconds: time.getSeconds(),
+      ampm: timeHours >= 12 ? "PM" : "AM",
+    }
+    setCurrentTime(timeToSet)
+  }
+
+  useEffect(() => {
+    setInterval(setTime, 1000)
+  }, [])
+
+  const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+  };
+
   return (
     <Draggable>
       <div className="widget widget-clock">
-        <h3>Clock Goes Here</h3>
+        <div class="clock">
+          <div class="needle hour" style={{ 
+            transform: `translate(-50%, -100%) 
+              rotate(${scale(currentTime.hoursForClock * 60 + currentTime.minutes, 0, 720, 0, 360)}deg)`,
+            transition: `${currentTime.hours === 0 ? "none" : "all 0.5s ease-in"}`
+          }}></div>
+          <div class="needle minute" style={{ 
+            transform: `translate(-50%, -100%) 
+              rotate(${scale(currentTime.minutes, 0, 60, 0, 360)}deg)`,
+            transition: `${currentTime.minutes === 0 ? "none" : "all 0.5s ease-in"}`
+          }}></div>
+          <div class="needle second" style={{ 
+            transform: `translate(-50%, -100%) 
+              rotate(${scale(currentTime.seconds, 0, 60, 0, 360)}deg)`,
+            transition: `${currentTime.seconds === 0 ? "none" : "all 0.5s ease-in"}`
+          }}></div>
+          <div class="center-point"></div>
+        </div>
+
+        <div class="time">
+          { currentTime.hoursForClock }:{ currentTime.minutes < 10 ? `0${currentTime.minutes}` : currentTime.minutes }
+        </div>
+        <div class="date">{ currentTime.day }, { currentTime.month } <span class="circle">{currentTime.date}</span></div>
       </div>
     </Draggable>
   )
